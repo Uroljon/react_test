ef989624649642e109709c2aa1b2c2e401f937db
Uroljon <uroljonkhidirboev@gmail.com>
2022-04-06 20:32:47 +0500

    Header component created test uchun qilindi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./Header";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello Uroljon</h1>
+      <Header />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
